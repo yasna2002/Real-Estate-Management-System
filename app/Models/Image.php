423cb811132eb6f568f78c21b2ab81cffeb31cc4
Ilya Shabanpour Foladi <ilyashabanpour@gmail.com>
2024-12-19 02:10:49 +0330

    hasFactory

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -3,9 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 
 class Image extends Model
 {
+    use HasFactory;
+
+
     protected $guarded = [
         'id',
     ];
--- app/Models/Property.php
+++ app/Models/Property.php
@@ -3,10 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 
 class Property extends Model
 {
+    use HasFactory;
+
     protected $guarded = [
         'id',
     ];
