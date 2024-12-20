3150bb0d4538dba7446683f6fcec912b2f95b4fd
Alireza Talebi <at@imagearts.de>
2024-12-20 16:40:33 +0100

    fixed x-select

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -2,6 +2,7 @@
     'name',
     'label',
     'options' => [],
+    'value' => null,
     'required' => false,
 ])
 
@@ -14,7 +15,9 @@
         {{ $attributes->merge(['class' => 'mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm']) }}>
         <option value="">Select {{ $label }}</option>
         @foreach ($options as $option)
-            <option value="{{ $option }}" {{ old($name) == $option ? 'selected' : '' }}>{{ $option }}</option>
+            <option value="{{ $option }}" {{ old($name, $value) == $option ? 'selected' : '' }}>
+                {{ $option }}
+            </option>
         @endforeach
     </select>
 </div>
